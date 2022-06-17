975303178c99eb3e5c0d512a932048cff68aa631
IronAsaf <asafdangoor@gmail.com>
2022-06-17 20:50:15 +0300

    fixed a bunch of stuff, react now loads signup pasge with the router

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import App from "./App";
-import Expenses from "./routes/expenses";
-import Invoices from "./routes/invoices";
+import SignIn from "./js/signin";
+import SignUp from "./js/signup";
+import Dashboard from "./js/dashboard";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root")
@@ -14,9 +14,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <Routes>
-    <Route path="/" element={<App />}>
-        <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+    <Route path="/" element={<SignUp />}>
+        <Route path="signin" element={<SignIn />} />
+        <Route path="dashboard" element={<Dashboard />} />
       </Route>
     </Routes>
   </BrowserRouter>
