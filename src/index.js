51eeaaa563891062d8f0db00818701aaaed3d872
IronAsaf <asafdangoor@gmail.com>
2022-06-17 20:31:14 +0300

    Some infor from tutorial of react router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 root.render(
-<BrowserRouter><App /></BrowserRouter>
+  <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
