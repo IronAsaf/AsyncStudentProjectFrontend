093eefb90ee5f68a9538c0a93058e5e73b27097b
IronAsaf <asafdangoor@gmail.com>
2022-06-24 20:33:28 +0300

    בךקשמ ופ

--- src/js/orders.js
+++ src/js/orders.js
@@ -4,28 +4,14 @@ import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import {Box, Grid, Typography} from "@mui/material";
-import {useState, useEffect} from "react";
-import {getExpensesById} from "./logic";
-import {useCredentials} from "./userAuthContext";
 import {useLocation} from "react-router-dom";
 
 const Orders = (props) => {
-
     const location = useLocation();
-
-    const {userId, password} = useCredentials();
-    const rows = props.rows;
-    const stats = props.stats;
-    const queryInfo = props.queryInfo;
-    const title = props.title;
     let id = 0;
 
-
-
-
-
     return (
-        <>{console.log(queryInfo)}
+        <>
             <Box p={1}>
                 <Grid>
                     <Grid p={1}>
